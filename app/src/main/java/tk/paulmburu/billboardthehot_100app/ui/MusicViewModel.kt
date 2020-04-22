5429084a845bdd53819efb9bcb602739498fa09b
Paulmburu <paulmburu53@gmail.com>
2020-04-22 23:44:25 +0300

    paging+coroutines-> retrieve songs

--- app/src/main/java/tk/paulmburu/billboardthehot_100app/ui/MusicViewModel.kt
+++ app/src/main/java/tk/paulmburu/billboardthehot_100app/ui/MusicViewModel.kt
@@ -1,92 +1,39 @@
 package tk.paulmburu.billboardthehot_100app.ui
 
-import android.app.Application
-import android.content.ContentValues
-import android.util.Log
-import androidx.lifecycle.AndroidViewModel
+
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import tk.paulmburu.billboardthehot_100app.Repository.FirebaseRepository
-import tk.paulmburu.billboardthehot_100app.data.models.Song
+import androidx.lifecycle.ViewModel
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import tk.paulmburu.billboardthehot_100app.data.SongsRepository
+import tk.paulmburu.billboardthehot_100app.data.models.RealtimeSong
+import tk.paulmburu.billboardthehot_100app.presentations.SongsDataSource
 
-class MusicViewModel(application: Application): AndroidViewModel(application){
-
-
-    val TAG = "MUSIC_VIEW_MODEL"
-    var firebaseRepository = FirebaseRepository()
-
-    val database = FirebaseDatabase.getInstance()
+class MusicViewModel(songsRepository: SongsRepository): ViewModel(){
 
 
-    var songs: MutableLiveData<List<Song>> = MutableLiveData()
+private val viewModelJob = SupervisorJob()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
-    fun getSongsUpdate():LiveData<List<Song>>{
-        Log.d(TAG,"firebase")
-        var currentSongs: MutableList<Song> = mutableListOf()
-        firebaseRepository.getBillboardSongs().addValueEventListener(object : ValueEventListener {
+    private val config = PagedList.Config.Builder()
+        .setEnablePlaceholders(false)
+        .setPrefetchDistance(10)
+        .setPageSize(20)
+        .build()
 
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-
-//                val post = dataSnapshot.getValue()
-
-                Log.d(TAG, "out for")
-                for (child in dataSnapshot.getChildren()) {
-                    Log.d(TAG, "in for")
-                    var artist_child: String = child.child("Artists").getValue().toString()
-                    var name_child: String = child.child("Name").getValue().toString()
-
-                    Log.d(TAG, "Value is: ")
-
-                    currentSongs.add(
-                        Song(
-                            artist_child,
-                            name_child
-                        )
-                    )
+    val records: LiveData<PagedList<RealtimeSong>> =
+        LivePagedListBuilder<String, RealtimeSong>(
+            SongsDataSource.Factory(songsRepository, uiScope),
+            config
+        ).build()
 
 
-                    Log.d(TAG, "Value is: {$name_child} == {$artist_child}")
-                }
-//                Log.d(TAG,currentSongs.toString())
-                songs.value = currentSongs
-
-            }
-
-
-
-
-            override fun onCancelled(error: DatabaseError) {
-                // Failed to read value
-                Log.d(ContentValues.TAG, "Failed to read value.", error.toException())
-            }
-        })
-//        Log.d(TAG,currentSongs.toString())
-        return songs
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
-
-
-//    suspend fun getMySongs(): DataSnapshot?{
-//        return  try {
-//            val data = database.getReference("Data").addValueEventListener()
-//        }
-//
-//    }
-
-
-    init {
-        Log.d(TAG,"We are in")
-       getSongsUpdate()
-    }
-//
-//    fun get_Songs(): LiveData<List<MusicSong>>{
-//        return songs
-//    }
-
 
 }
--- app/src/main/java/tk/paulmburu/billboardthehot_100app/ui/MusicViewModelFactory.kt
+++ app/src/main/java/tk/paulmburu/billboardthehot_100app/ui/MusicViewModelFactory.kt
@@ -2,14 +2,16 @@ package tk.paulmburu.billboardthehot_100app.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import tk.paulmburu.billboardthehot_100app.data.SongsRepository
 
 class MusicViewModelFactory(
-//    private val dataSource: TheUserDatabaseDao): ViewModelProvider.Factory{
-//    @Suppress("unchecked_cast")
-//    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-//        if (modelClass.isAssignableFrom(VerifyNumberViewModel::class.java)) {
-//            return VerifyNumberViewModel(theUserKey, dataSource) as T
-//        }
-//        throw IllegalArgumentException("Unknown ViewModel class")
-//    }
-)
+    private val songsRepository: SongsRepository
+): ViewModelProvider.Factory{
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MusicViewModel::class.java)) {
+            return MusicViewModel(songsRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
