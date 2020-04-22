4d0f0668d9d1dbf8602b593a1d5a28cfe47fb453
Paulmburu <paulmburu53@gmail.com>
2020-04-22 23:46:08 +0300

    display songs on the recycler view

--- app/src/main/java/tk/paulmburu/billboardthehot_100app/ui/MainActivity.kt
+++ app/src/main/java/tk/paulmburu/billboardthehot_100app/ui/MainActivity.kt
@@ -2,48 +2,45 @@ package tk.paulmburu.billboardthehot_100app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import tk.paulmburu.billboardthehot_100app.Adapters.MyAdapter
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import tk.paulmburu.billboardthehot_100app.R
+import tk.paulmburu.billboardthehot_100app.data.SongsRepository
+import tk.paulmburu.billboardthehot_100app.presentations.SongsAdapter
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    lateinit var factory: MusicViewModelFactory
+    lateinit var viewModel: MusicViewModel
     private lateinit var viewManager: RecyclerView.LayoutManager
 
+    val swipeSongs by lazy { findViewById<SwipeRefreshLayout>(R.id.swipe_songs)  }
+    val recyclerViewSongs by lazy { findViewById<RecyclerView>(R.id.my_recycler_view) }
+    val songsAdapter by lazy { SongsAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val application = requireNotNull(this).application
-
-        val viewModel = ViewModelProviders.of(this)[MusicViewModel(
-            application
-        )::class.java]
-
-
-
+        factory = MusicViewModelFactory(SongsRepository())
         viewManager = LinearLayoutManager(this)
-        viewAdapter = MyAdapter(viewModel.songs)
 
-
-        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
-            // use this setting to improve performance if you know that changes
-            // in content do not change the layout size of the RecyclerView
-            setHasFixedSize(true)
-
+        recyclerViewSongs.apply {
             // use a linear layout manager
             layoutManager = viewManager
 
             // specify an viewAdapter (see also next example)
-            adapter = viewAdapter
+            adapter = songsAdapter
         }
+        viewModel = ViewModelProviders.of(this, factory).get(MusicViewModel::class.java)
 
-
+        viewModel.records.observe(this, Observer {
+            swipeSongs.isRefreshing = false
+            songsAdapter.submitList(it)
+        })
 
     }
 }
